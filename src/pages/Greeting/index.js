513de57435badbf6ee7469d7e4975017bce855b3
Gabriel <knuckles4x@gmail.com>
2021-02-03 14:55:58 -0300

    cover

--- src/pages/Greeting/index.js
+++ src/pages/Greeting/index.js
@@ -4,10 +4,18 @@ import { bindActionCreators } from 'redux';
 
 import * as CartActions from '../../store/modules/cart/actions';
 
-import { Container } from './styles';
+import { Container, Cover } from './styles';
 
 function Greeting() {
-  return <Container />;
+  return (
+    <Container>
+      <Cover imgSrc="https://scontent.fcgh37-1.fna.fbcdn.net/v/t1.0-9/31271238_10156405833644884_5060567756500369408_o.jpg?_nc_cat=111&ccb=2&_nc_sid=e3f864&_nc_eui2=AeFOQebyqbBjV4eQNY4rpkPlQOAHvxzOl4xA4Ae_HM6XjNa-Z1-igBUHKh1NoG7J301Yr-UFYEy-dfV8b6d1P2JO&_nc_ohc=9T4OCyDhMw4AX-dMtxr&_nc_ht=scontent.fcgh37-1.fna&oh=307cc5adafda95225bb13ab36628e610&oe=604199A0">
+        <div className="cover-container">
+          <figure />
+        </div>
+      </Cover>
+    </Container>
+  );
 }
 
 const mapStateToProps = (state) => ({
--- src/pages/Greeting/styles.js
+++ src/pages/Greeting/styles.js
@@ -1,3 +1,35 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.main``;
+
+export const Cover = styled.div`
+  margin-top: 100px;
+  width: 100vw;
+  height: 56.25vw;
+  max-height: calc(100vh - 100px);
+  position: relative;
+
+  .cover-container {
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    -webkit-box-pack: center;
+    justify-content: center;
+    -webkit-box-align: center;
+    align-items: center;
+    background: rgba(34, 34, 34, 0.3);
+    pointer-events: none;
+    z-index: 1;
+
+    > figure {
+      width: 100%;
+      height: 100%;
+      background-image: ${(props) => `url(${props.imgSrc})`};
+      background-size: cover;
+    }
+  }
+`;
